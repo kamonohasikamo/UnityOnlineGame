406b436360737ac55549ec519808a0183a605e67
kamonohasikamo <kamonohasikamo@users.noreply.github.com>
2019-07-21 23:43:24 +0900

    create Network init

---
+++ Assets/Script/GameTitleScript/ConnectionScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ConnectionScript : Photon.PunBehaviour {
+	private string gameVersion = "testGameVersion";
+
+	public void Connect() {
+		if(!PhotonNetwork.connected) {
+			PhotonNetwork.ConnectUsingSettings(gameVersion);
+			Debug.Log("Photonに接続");
+		}
+	}
+
+	// Photon コールバック
+	public override void OnJoinedLobby() {
+		Debug.Log("into Lobby");
+		// Randomで部屋選び・部屋に入る
+		// 部屋がなければOnPhotonRandomJoinFailedが呼ばれる
+		PhotonNetwork.JoinRandomRoom();
+	}
+
+	public override void OnPhotonRandomJoinFailed(object[] codeAndMsg) {
+		Debug.Log("ルームに入れませんでした。");
+		PhotonNetwork.CreateRoom("TestRoom");
+	}
+
+	public override void OnJoinedRoom() {
+		Debug.Log("Room に入りました。");
+		PhotonNetwork.LoadLevel("Game");
+	}
+
+}
---
+++ Assets/Script/GameTitleScript/InputNameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputNameController : MonoBehaviour {
+	private static string playerNameKey = "PlayerName";
+
+	void Start() {
+		string defaultName = "";
+		InputField inputNameField = this.GetComponent<InputField>();
+
+		//前回プレイ開始時に入力した名前をロードして表示
+		if (inputNameField != null) {
+			if (PlayerPrefs.HasKey(playerNameKey)) {
+				defaultName = PlayerPrefs.GetString(playerNameKey);
+				inputNameField.text = defaultName;
+			}
+		}
+	}
+
+	public void setPlayerName(string value) {
+		PhotonNetwork.playerName = value + " "; // player Name setting
+		PlayerPrefs.SetString(playerNameKey, value); // player Name saving
+		Debug.Log("PlayerName = " + PhotonNetwork.playerName); // player Name checking
+	}
+
+}
